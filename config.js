4fc5e8342b30f26c8301f87ebb3b000e3e39ac86
ChanPo-Po <tp139365@gmail.com>
2026-09-21 18:06:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyVE8SFWyYkLnCLp9gINjphGtus2kDiewyXiTbktNw9LuSNZ9MtNCaCnJS9R7fgE_2P/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbxyqs97N9VwzR8aCikLBrZmdBmneh1v4NzWo7wIny7xoIqRwUQI349kPEceIhpVtxc/exec"
 };
